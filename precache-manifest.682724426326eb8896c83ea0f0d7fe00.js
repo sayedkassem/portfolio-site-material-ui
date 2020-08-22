7001d89e4ff2c480135c29202f72d70af92ab7a3
Sayed Kassem <sayedkassem-9@outlook.com>
2020-08-22 15:27:42 +0300

    Updates

---
+++ precache-manifest.682724426326eb8896c83ea0f0d7fe00.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f97a3f93abd28210dbc23f9ca47ee0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58046d41851969a743c7",
+    "url": "/static/css/main.0368ebfe.chunk.css"
+  },
+  {
+    "revision": "63622e295a5061b04729",
+    "url": "/static/js/2.7f08737b.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/static/js/2.7f08737b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58046d41851969a743c7",
+    "url": "/static/js/main.dc5eefeb.chunk.js"
+  },
+  {
+    "revision": "816a7b29e840a5c14c95",
+    "url": "/static/js/runtime-main.94f82867.js"
+  },
+  {
+    "revision": "697a75897564ad60fbbc6aa8e9796789",
+    "url": "/static/media/WeatherApp.697a7589.png"
+  },
+  {
+    "revision": "9de8b126eaf55bb1fe6082e0c986a7d2",
+    "url": "/static/media/avatar.9de8b126.png"
+  },
+  {
+    "revision": "0cd097665b54d4b9bcde4e7890afa7ad",
+    "url": "/static/media/bootstrap.0cd09766.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "1bded2d6401613eedb4f4263c877b4bb",
+    "url": "/static/media/taskManager.1bded2d6.png"
+  }
+]);
